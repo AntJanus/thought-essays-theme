ca85a9303cb7456df28ce538817d40c22a2229ec
Antonin Januska <antonin@antjanus.com>
2013-10-08 08:19:10 -0500

    Removal of extra code

--- 404.php
+++ 404.php
@@ -1,8 +1,7 @@
 <?php theme_include('header'); ?>
 
 <div class="container" id="content">
-  <!-- Example row of columns -->
-  <div class="row">
+ <div class="row">
     <div class="col-md-2 visible-md visible-lg">
         &nbsp;
     </div>
@@ -15,10 +14,6 @@
     <?php theme_include('sidebar'); ?>
 </aside>
 </div>
-<hr />
-<p>
-    Created by <a href="http://antjanus.com">AntJanus</a>
-</p>
 </div>
 
 <?php theme_include('footer'); ?>
