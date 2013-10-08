11da520b27684742f58089c75667c37a5eb39d1d
Antonin Januska <antonin@antjanus.com>
2013-10-08 08:20:19 -0500

    Commenting abstraction

--- article.php
+++ article.php
@@ -33,27 +33,13 @@
 src="//pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>
 </div>
-<div id="disqus_thread"></div>
-<script type="text/javascript">
-/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'thoughtessays'; // required: replace example with your forum shortname
-
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-          dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-        </script>
-        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-
-      </div>
-      <aside class="col-md-3" id="sidebar">
-        <?php theme_include('sidebar'); ?>
-      </aside>
-    </div>
-  </div>
+<?php theme_include('comments') ?>
+</div>
+<aside class="col-md-3" id="sidebar">
+  <?php theme_include('sidebar'); ?>
+</aside>
+</div>
+</div>
 </div>
 
 <?php theme_include('footer'); ?>
